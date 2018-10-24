5d689270a1c8e6549b015c1865ddd53162a75b71
candescent-flow-1570 <f.iqbal.w@gmail.com>
2018-10-24 19:25:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-function happyHolidaysTo(holiday, name) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
